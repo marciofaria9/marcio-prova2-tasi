3360b186d8598b48ab606c6ad3430c3785bbbbca
Marcio Faria <m.faria.jr@icloud.com>
2023-06-25 15:58:26 -0300

    finalizando a prova

--- marcio_faria_integration_test/src/main/java/MaterialTest.java
+++ marcio_faria_integration_test/src/main/java/MaterialTest.java
@@ -3,15 +3,13 @@ import static org.junit.Assert.assertEquals;
 import org.apache.http.HttpStatus;
 import org.junit.Test;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import DTO.MaterialDTO;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 public class MaterialTest {
-	private ObjectMapper mapper = new ObjectMapper();
+
 	private final String materialURL = "http://localhost:8080/materials";
 
 	@Test
@@ -22,7 +20,7 @@ public class MaterialTest {
 
 	@Test
 	public void testCreateMateiralExisting() { // Teste criando material que já existe
-		
+
 		createMaterial();
 		Response resp = createMaterial();
 		resp.then().assertThat().statusCode(HttpStatus.SC_BAD_REQUEST);
@@ -35,35 +33,35 @@ public class MaterialTest {
 	}
 
 	@Test
-	public void testCreatematerialInvalid() { //Teste Criando com Dado inválido (sem provider)
+	public void testCreatematerialInvalid() { // Teste Criando com Dado inválido (sem provider)
 		Response resp = createMaterialWithoutProvider();
 		resp.then().assertThat().statusCode(HttpStatus.SC_BAD_REQUEST);
 	}
-	
+
 	private Response createMaterialWithoutProvider() {
 		MaterialDTO material = new MaterialDTO(1, "Teste", 2f, null, "01-01-2000", true);
 		Response resp = RestAssured.given().body(material).contentType(ContentType.JSON).post(materialURL);
 		return resp;
 	}
-	
+
 	@Test
-	public void testGetMaterialById() {  // Teste get by id com sucesso
+	public void testGetMaterialById() { // Teste get by id com sucesso
 		Integer materialId = 1;
 		createMaterial();
 		Response resp = RestAssured.get(materialURL + materialId);
 		resp.then().assertThat().statusCode(HttpStatus.SC_OK);
 	}
-	
-	public void testGetMaterialByIdInexisting () { // Teste get by id Inexistente
+
+	public void testGetMaterialByIdInexisting() { // Teste get by id Inexistente
 		Integer inexistingMaterialId = 2;
 		createMaterial();
-		Response resp = RestAssured.get(materialURL + inexistingMaterialId); 
+		Response resp = RestAssured.get(materialURL + inexistingMaterialId);
 		resp.then().assertThat().statusCode(HttpStatus.SC_NOT_FOUND);
-		
+
 	}
-	
+
 	@Test
-	public void testActive() {  // Teste do active com sucesso
+	public void testActive() { // Teste do active com sucesso
 		Integer code = 1;
 		createMaterial();
 		Response action = RestAssured.put(materialURL + "active/" + code);
@@ -73,6 +71,18 @@ public class MaterialTest {
 		Response resp = RestAssured.get(materialURL + code);
 		resp.then().assertThat().statusCode(HttpStatus.SC_OK);
 	}
-	
-	
+
+	public void testActiveWithError() { // Teste do active sem sucesso
+		Integer materialId = 1;
+		createMaterial();
+
+		Response resp = RestAssured.get(materialURL + materialId);
+		if (resp.getStatusCode() == HttpStatus.SC_OK) {
+			Response action = RestAssured.put(materialURL + "active/" + materialId);
+			action.then().assertThat().statusCode(HttpStatus.SC_NO_CONTENT);
+			assertEquals(HttpStatus.SC_NO_CONTENT, action.getStatusCode());
+		} else {
+			resp.then().assertThat().statusCode(HttpStatus.SC_NOT_FOUND);
+		}
+	}
 }
